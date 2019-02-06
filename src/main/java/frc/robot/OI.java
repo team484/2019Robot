1bdfdd6134b8a9cda36c93c0200f7bc19b74528d
niev ano <noahd912@gmail.com>
2019-02-05 19:12:46 -0500

    adds hella buttons

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,40 +7,45 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.*;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+  
+  private static Button pickupCargo;
+  private static Button shootCargo;
+  private static Button extendHatch;
+  private static Button grabHatch;
+  private static Button raiseIntake;
+  private static Button releaseHatch;
+  private static Button retractHatch;
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
-
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
-
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
-
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
 
   public void setupOI() {
-    //Add joystick button mappings here
+    
+    pickupCargo = new JoystickButton(RobotIO.operatorStick, 0);
+    shootCargo = new JoystickButton(RobotIO.operatorStick, 0);
+    extendHatch = new JoystickButton(RobotIO.operatorStick, 0);
+    grabHatch = new JoystickButton(RobotIO.operatorStick, 0);
+    raiseIntake = new JoystickButton(RobotIO.operatorStick, 0);
+    releaseHatch = new JoystickButton(RobotIO.operatorStick, 0);
+    retractHatch = new JoystickButton(RobotIO.operatorStick, 0);
+
+    pickupCargo.whenPressed(new PickupCargo());
+    shootCargo.whenPressed(new ShootCargo());
+    extendHatch.whenPressed(new ExtendHatch());
+    grabHatch.whenPressed(new GrabHatch());
+    raiseIntake.whenPressed(new RaiseIntake());
+    releaseHatch.whenPressed(new ReleaseHatch());
+    retractHatch.whenPressed(new RetractHatch());
+
+
+
   }
 }
